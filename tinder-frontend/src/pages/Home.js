c83bcf8186d158d964a523da4cb8c225cb29a0a5
Steve Berg <59520287+SteveB98@users.noreply.github.com>
2023-11-27 23:35:43 -0800

    Implementing overlay blur effect with AuthModal

--- tinder-frontend/src/pages/Home.js
+++ tinder-frontend/src/pages/Home.js
@@ -1,34 +1,49 @@
 import Nav from "../components/Nav";
 import { useState } from "react";
+import React from "react";
 import AuthModal from "../components/AuthModal";
+import '../index.css';
 
 const Home = () => {
   const [showModal, setShowModal] = useState(false);
   const [isSignUp, setIsSignUp] = useState(true);
-  
+  const [isModalActive, setIsModalActive] = useState(false);
   const authToken = false;
 
   const handleClick = () => {
     console.log("clicked");
-    setShowModal(true);
     setIsSignUp(true);
   };
 
+  const handleModalOpen = () => {
+    setIsModalActive(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalActive(false);
+  };
+  
   return (
-    <div className="overlay">
+    <div className={`overlay ${isModalActive ? 'blur' : ''}` }>
       <Nav
         minimal={false}
         setShowModal={setShowModal}
         showModal={showModal}
         setIsSignUp={setIsSignUp}
+        isSignUp={isSignUp}
+        onClose={handleModalClose}
       />
       <div className="home">
         <h1 class="slideInLeft" className="primary-title">Swipe Right</h1>
-        <button className="primary-button" onClick={handleClick}>
+        <button className="primary-button"
+         onClick={() => {handleClick();
+                        handleModalOpen();
+                        setShowModal(true);
+         }}>
           {authToken ? "Signout" : "Create Account"}
         </button>
         {showModal && (
-          <AuthModal setShowModal={setShowModal} isSignUp={isSignUp} />
+          <AuthModal showModal={showModal} setShowModal={setShowModal} isSignUp={isSignUp} onClose={handleModalClose} />
         )}
       </div>
     </div>
